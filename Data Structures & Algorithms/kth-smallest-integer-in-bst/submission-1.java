3f170f90381dbb44759dfdca6b0bfe740ea78174
Tejas Drolia <57807735+tejasdrolia@users.noreply.github.com>
2026-06-10 23:19:04 +0530

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> arr = new ArrayList<>();
+
+        dfs(root, arr);
+        return arr.get(k - 1);
+    }
+
+    private void dfs(TreeNode node, List<Integer> arr) {
+        if (node == null) {
+            return;
+        }
+
+        dfs(node.left, arr);
+        arr.add(node.val);
+        dfs(node.right, arr);
+    }
+}
