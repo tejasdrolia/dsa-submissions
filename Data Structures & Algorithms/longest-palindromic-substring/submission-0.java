b20e47c723cc89db0d49fa5b48445d130a7301e7
Tejas Drolia <57807735+tejasdrolia@users.noreply.github.com>
2026-06-12 16:32:38 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        // 2d array
+        boolean dp[][] = new boolean[s.length()][s.length()];
+        String ans ="";
+        for(int g = 0;g<s.length();g++)
+        {
+            for(int i = 0,j = g;j<s.length();i++,j++)
+            {
+                if(g==0)
+                {
+                    dp[i][j] = true;
+                    ans = s.substring(i,j+1);
+                }
+                else if(g==1)
+                {
+                    if(s.charAt(i) == s.charAt(j))
+                    {
+                        dp[i][j] = true;
+                        ans = s.substring(i,j+1);
+                    }
+                    else
+                    dp[i][j] = false;
+                }
+                else
+                {
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1])
+                    {
+                        dp[i][j] = true;
+                        ans = s.substring(i,j+1);
+                    }
+                    else
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return ans;
+    }
+}
