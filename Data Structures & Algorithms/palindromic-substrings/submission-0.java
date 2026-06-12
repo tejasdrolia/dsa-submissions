36c79b7b9fd2ee349c0cf865107e182ab5087f5f
Tejas Drolia <57807735+tejasdrolia@users.noreply.github.com>
2026-06-12 16:35:59 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int countSubstrings(String s) {
+        // 2d array
+        boolean dp[][] = new boolean[s.length()][s.length()];
+        int count  = 0;
+        for(int g = 0;g<s.length();g++)
+        {
+            for(int i = 0,j = g;j<s.length();i++,j++)
+            {
+                if(g==0)
+                {
+                    dp[i][j] = true;
+                    count++;
+                }
+                else if(g==1)
+                {
+                    if(s.charAt(i) == s.charAt(j))
+                    {
+                        dp[i][j] = true;
+                        count++;
+                    }
+                    else
+                    dp[i][j] = false;
+                }
+                else
+                {
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1])
+                    {
+                        dp[i][j] = true;
+                        count++;
+                    }
+                    else
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return count;
+    }
+}
