91a41204623cd913d0ef9e82ab227f8b86e4b505
Tejas Drolia <57807735+tejasdrolia@users.noreply.github.com>
2026-06-22 13:04:16 +0530

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if(Arrays.stream(gas).sum() < Arrays.stream(cost).sum())
+        return -1;
+
+        int total = 0;
+        int cans = 0;
+        for(int i = 0;i<gas.length;i++)
+        {
+            total += (gas[i] - cost[i]);
+            if(total<0)
+            {
+                total = 0;
+                cans = i+1;
+            }
+        }
+        return cans;
+    }
+}
