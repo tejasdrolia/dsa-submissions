c934100152ad186eca39ecce6febcc179f82b8ed
Tejas Drolia <57807735+tejasdrolia@users.noreply.github.com>
2026-06-24 16:57:04 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        int k = n;
+        HashSet<Integer> set = new HashSet<>();
+        while(k!=1)
+        {
+            //System.out.println(k);
+            if(set.size()>0 && set.contains(k))
+            return false;
+            set.add(k);
+            int temp = 0;
+            String s = Integer.toString(k);
+            for(int i = 0;i<s.length();i++)
+            {
+                temp+= Math.pow(Integer.parseInt(s.charAt(i)+""),2);
+            }
+            k = temp;
+        }
+        return true;
+    }
+}
