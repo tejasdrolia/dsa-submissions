ce4802b93d08bf7e0c11b9f05c1771f9f47340b8
Tejas Drolia <57807735+tejasdrolia@users.noreply.github.com>
2026-07-04 08:31:38 +0530

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,8 @@
+SELECT seller_name
+FROM seller
+WHERE seller_id NOT IN (
+    SELECT seller_id
+    FROM orders
+    WHERE sale_date >= '2020-01-01' AND sale_date <= '2020-12-31'
+)
+ORDER BY seller_name;
